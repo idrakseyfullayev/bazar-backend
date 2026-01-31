a70cd13995ab69c594abfb40dcdf8e9076eb1133
idrakseyfullayev <idrak.seyfi87@gmail.com>
2026-01-31 19:24:23 +0400

    Use PostgreSQL instead of SQLite

--- bazar/settings.py
+++ bazar/settings.py
@@ -91,12 +91,14 @@ WSGI_APPLICATION = "bazar.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+import os
+import dj_database_url  # pip install dj-database-url lazım ola bilər
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
